6a5b16de4b0ffab07812f155d4757d5687289d88
markcampbell102 <43571712+markcampbell102@users.noreply.github.com>
2019-02-20 15:17:37 +0000

    added Number of Hotspots varable
    added a way to select how many random hotspots would be added to the heatmap

--- Matlab/initalise_heatmap_2.m
+++ Matlab/initalise_heatmap_2.m
@@ -4,40 +4,34 @@ Range_min = 0;
 Range_max = 10;
 SD_min = 1;
 SD_max = 5;
-% Number_of_Hotspots = 2;
+Number_of_Hotspots = 2;
+Mat_Size = (((Range_max-Range_min)*(1/Resolution))+1)^2;
+out = zeros(Mat_Size,1);
+% size(test)
+
+%% old
+% %% random number genarators
+% Pos_X_1 = randi([Range_min Range_max],1);
+% Pos_X_2 = randi([Range_min Range_max],1);
+% Pos_Y_1 = randi([Range_min Range_max],1);
+% Pos_Y_2 = randi([Range_min Range_max],1);
+% Shape_X_1 = randi([SD_min SD_max],1);
+% Shape_X_2 = randi([SD_min SD_max],1);
+% Shape_Y_1 = randi([SD_min SD_max],1);
+% Shape_Y_2 = randi([SD_min SD_max],1);
 % 
-% for a=Number_of_Hotspots
-%     Pos_X = randi([Range_min Range_max],1);
-%     Pos_Y = randi([Range_min Range_max],1);
-%     Shape_X = randi([SD_min SD_max],1);
-%     Shape_Y = randi([SD_min SD_max],1);
-%     Pos = [Pos_X_1 Pos_Y_1];
-%     Shape = [Shape_X_1 .3; .3 Shape_Y_1];
-%     
-% end
-
-%% random number genarators
-Pos_X_1 = randi([Range_min Range_max],1);
-Pos_X_2 = randi([Range_min Range_max],1);
-Pos_Y_1 = randi([Range_min Range_max],1);
-Pos_Y_2 = randi([Range_min Range_max],1);
-Shape_X_1 = randi([SD_min SD_max],1);
-Shape_X_2 = randi([SD_min SD_max],1);
-Shape_Y_1 = randi([SD_min SD_max],1);
-Shape_Y_2 = randi([SD_min SD_max],1);
-
-%% hot spots info
-mu1 = [9 9];                            % posision [X Y]
-Sigma1 = [1 .3; .3 3];                  % shape [(width of x) (?) ; (?) (width of y)]
-
-mu2 = [5 5];                            % posision [X Y]
-Sigma2 = [3 .3; .3 3];                  % shape [(width of x) (?) ; (?) (width of y)]
-
-Pos_1 = [Pos_X_1 Pos_Y_1];
-Shape_1 = [Shape_X_1 .3; .3 Shape_Y_1];
-
-Pos_2 = [Pos_X_2 Pos_Y_2];
-Shape_2 = [Shape_X_2 .3; .3 Shape_Y_2];
+% %% hot spots info
+% mu1 = [9 9];                            % posision [X Y]
+% Sigma1 = [1 .3; .3 3];                  % shape [(width of x) (?) ; (?) (width of y)]
+% 
+% mu2 = [5 5];                            % posision [X Y]
+% Sigma2 = [3 .3; .3 3];                  % shape [(width of x) (?) ; (?) (width of y)]
+% 
+% Pos_1 = [Pos_X_1 Pos_Y_1];
+% Shape_1 = [Shape_X_1 .3; .3 Shape_Y_1];
+% 
+% Pos_2 = [Pos_X_2 Pos_Y_2];
+% Shape_2 = [Shape_X_2 .3; .3 Shape_Y_2];
 
 %% Range???
 x = Range_min:Resolution:Range_max;     % [low lim : resalution : up lim]
@@ -46,29 +40,51 @@ y = Range_min:Resolution:Range_max;
 %%
 [X,Y] = meshgrid(x,y);                  % ???
 
+%% slect number of hotspots on map...
+% F1 = mvnpdf([X(:) Y(:)],mu1,Sigma1);    % tall
+% 
+% size(F1)
+% out = zeros(size(F1));
+for a=1:Number_of_Hotspots
+    % random
+    Pos_X = randi([Range_min Range_max],1);
+    Pos_Y = randi([Range_min Range_max],1);
+    Shape_X = randi([SD_min SD_max],1);
+    Shape_Y = randi([SD_min SD_max],1);
+    Pos = [Pos_X Pos_Y];
+    Shape = [Shape_X .3; .3 Shape_Y];
+    
+    % make hotspot
+    test = mvnpdf([X(:) Y(:)],Pos,Shape);
+    out = out + test;
+end
+
 %% hot spots
-% inital functions
-F1 = mvnpdf([X(:) Y(:)],mu1,Sigma1);    % tall
-F2 = mvnpdf([X(:) Y(:)],mu2,Sigma2);    % fat
+% % inital functions
+% F1 = mvnpdf([X(:) Y(:)],mu1,Sigma1);    % tall
+% F2 = mvnpdf([X(:) Y(:)],mu2,Sigma2);    % fat
+% 
+% F = F1 + F2;                            % add a number (2) of "hotspots"  (check how merge???)
+% 
+% % actual hotspots
+% H1 = mvnpdf([X(:) Y(:)],Pos_1,Shape_1);
+% H2 = mvnpdf([X(:) Y(:)],Pos_2,Shape_2);
+% 
+% H = H1 + H2;
 
-F = F1 + F2;                            % add a number (2) of "hotspots"  (check how merge???)
-
-% actual hotspots
-H1 = mvnpdf([X(:) Y(:)],Pos_1,Shape_1);
-H2 = mvnpdf([X(:) Y(:)],Pos_2,Shape_2);
-
-H = H1 + H2;
+% test
+Hot_Spots = out;
 
 %% reeshape???
-H = reshape(H,length(y),length(x));
+Hot_Spots = reshape(Hot_Spots,length(y),length(x));
 
 
 %% plots
 % 3d
 figure;
-s = surf(x,y,H);
-set(s,'edgecolor','none');
-caxis([min(H(:))-.5*range(H(:)),max(H(:))]);
+Surf_plot = surf(x,y,Hot_Spots);
+set(Surf_plot,'edgecolor','none');
+caxis([min(Hot_Spots(:))-.5*range(Hot_Spots(:)),max(Hot_Spots(:))]);
 %axis([0 10 0 10 0 .1])
 
 % lable
@@ -79,7 +95,7 @@ zlabel('Probability Density');
 
 % heatmap
 figure;
-h = heatmap(H);
-h.GridVisible = 'off';
+Heat_map = heatmap(Hot_Spots);
+Heat_map.GridVisible = 'off';
 colormap('jet');
 
