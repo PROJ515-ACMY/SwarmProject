2ff16c4e5833c8004be7463fb6e7d43758ca16ea
markcampbell102 <43571712+markcampbell102@users.noreply.github.com>
2019-04-19 22:25:48 +0100

    Altered requirements for interpulation and checking when finished
    after checking with argos found need to make sure there are 3 points for triangulation... will slow start for linear lines but should work for all senarios....
    
    also the last bit was set at the wrong time so altered so it would work.
    
    version to go to other github...

--- Matlab/Useful_Files/make_heatmap_V2.m
+++ Matlab/Useful_Files/make_heatmap_V2.m
@@ -10,35 +10,35 @@ f3 = figure('Name','Heatmap','NumberTitle','off');
 filename_1 = 'Current_Data.txt';                                            % CSV x,y,v x,y= copradanates v= value
 filename_2 = 'Start_Stopped.txt';
 
-%% poll start stop file
+%% Poll start stop file so know when to start
 while loop == 1
-    % read file
+    % Read file
     [Start, ~, ~, filename_1] = textread(filename_2, '%d %d %d %s');
     filename_1 = char(filename_1);
 
     if Start == 1
-        % exit while loop
+        % Exit waiting to begin loop
         loop = 0;
     end
 end
 
-%% Loop until done
+%% Loop until mapping is finished
 loop = 1;
 while loop == 1
 
     %% Get data
-    % read file
+    % Read file
     File_Data_1 = csvread(filename_1);
     
-    % file size
+    % work out the size of the file
     [a1,a2] = size(File_Data_1);
     [b1,b2] = size(File_Data_1_old);
 
-    %% check if file has updated
-    if a1 ~= b1         % a1 > 0 && 
+    %% Check if file has changed size (been updated)
+    if a1 ~= b1
         
-        %% alter the data to work with matlab
-        % resolution...
+        %% Alter the data to work with matlab
+        % Set the Resolution values
         if Resolution_decimal_place == 0
             Resolution = 1;
             Multiplier = 1;
@@ -57,36 +57,36 @@ while loop == 1
             Multiplier = 100;
         end
         
-        % offsets...
+        % Work out a small offset
         x_off = abs(min(File_Data_1(:,1))) + Resolution;
         y_off = abs(min(File_Data_1(:,2))) + Resolution;
         
-        % change from range -X,Y to 0,d+(X+a small amount (as doesnt like 0))
+        % Change from range -X,Y to 0,X+a small offset (as doesnt like 0)
         File_Data_1(:,2) = (File_Data_1(:,2) + y_off);
         File_Data_1(:,1) = (File_Data_1(:,1) + x_off);
         
-        % round data to resaloution required
+        % Round data to resaloution required
         File_Data_1(:,2) = round(File_Data_1(:,2), Resolution_decimal_place);
         File_Data_1(:,1) = round(File_Data_1(:,1), Resolution_decimal_place);
         
-        % change data from float to int...
+        % Change data from float to int
         File_Data_1(:,2) = File_Data_1(:,2) * Multiplier;
         File_Data_1(:,1) = File_Data_1(:,1) * Multiplier;
         
-        % fix remaining floats that refuse to be ints??? (15.000000)
+        % Fix remaining floats that refuse to be ints??? (15.000000)
         File_Data_1(:,2) = fix(File_Data_1(:,2));
         File_Data_1(:,1) = fix(File_Data_1(:,1));
         
-        % area whitch has been sampeled
+        % Area whitch has been sampeled
         Range_min_X = min(File_Data_1(:,1));
         Range_max_X = max(File_Data_1(:,1));
         Range_min_Y = min(File_Data_1(:,2));
         Range_max_Y = max(File_Data_1(:,2));
 
-        %% make raw heatmap
+        %% Make raw heatmap
         Raw_Heatmap_Data = zeros(Range_max_Y,Range_max_X);
         
-        % rearange data
+        % Rearange data
         Raw_Heatmap_Data(sub2ind(size(Raw_Heatmap_Data),(File_Data_1(:,2)),(File_Data_1(:,1)))) = File_Data_1(:,3);
         
         %% Interpolate
@@ -98,6 +98,7 @@ while loop == 1
         y = Range_min_Y:1:Range_max_Y;
         [X,Y] = meshgrid(x,y);
 
+        % Work out a "Value" for each coardanate
         Value = F(X,Y);                                                             % Maps each coardanate with a Value
 
         %% Plots
@@ -108,12 +109,12 @@ while loop == 1
         Raw_Heat_map.GridVisible = 'off';                                           % Remove gridlines
         colormap('jet');                                                            % Set colour scale 'jet'= clearest for humans
 
-        % check if there are more than 2 coloms or rows filled so can be (interpolated/triangulated)
+        % Check if there are more than 2 coloms or rows filled so can be (interpolated/triangulated)
         filled_columns = any(Raw_Heatmap_Data,1);
         filled_rows = any(Raw_Heatmap_Data,2);
         num_of_filled_columns = sum(filled_columns(:) == 1);
         num_of_filled_rows = sum(filled_rows(:) == 1);
-        if num_of_filled_columns > 1 && num_of_filled_rows > 1
+        if num_of_filled_columns > 2 && num_of_filled_rows > 2
             % Interpolated
             figure(f3);
             Int_Heat_map = heatmap(x,y,flip(Value));
@@ -133,27 +134,26 @@ while loop == 1
             title('Natural neighbor Interpolation Method','fontweight','b');
         end
         
-        %% repeat
+        %% Prepare to loop for next set of data
         drawnow;
         File_Data_1_old = File_Data_1;
+
+    end
+    
+    %% Check if the last reading has been sent
+    [~, Last, ~, ~] = textread(filename_2, '%d %d %d %s');
+    if Last == 1
+        %% Tell argos mapping is finished
+        Finish = 1;
+        File_Data_2 = [Start, Last, Finish, filename_1];
         
-        %% check if last reading
-        [~, Last, ~, ~] = textread(filename_2, '%d %d %d %s');
-%         File_Data_2 = csvread(filename_2);
-        if Last == 1
-            %% tell argos finished
-            Finish = 1;
-            File_Data_2 = [Start, Last, Finish, filename_1];
-%             File_Data_2(2,1) = 1;
-            
-            % write file
-            fileID = fopen(filename_2, 'w');
-            fprintf(fileID, '%d %d %d %s', File_Data_2);
-            fclose(fileID);
-            
-            % exit while loop
-            loop = 0;
-        end
+        % Write over file
+        fileID = fopen(filename_2, 'w');
+        fprintf(fileID, '%d %d %d %s', File_Data_2);
+        fclose(fileID);
+        
+        % Exit the loop to end
+        loop = 0;
     end
 end
 
